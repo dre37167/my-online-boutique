b9d54f28c917babfebd281edca8195bb11ab8115
dre37167 <tidre37167@gmail.com>
2019-01-08 16:36:24 -0700

    fix cart add-btn & added functional removed items button

--- src/Features/product-listing/product-list-item.js
+++ src/Features/product-listing/product-list-item.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import AddBtn from './add-btn';
+import RemoveBtn from './remove-btn';
 
 export default function ProductListItem(props) {
-  const thisItemInCart = props.cart.filter(item => item.id === props.product.id)[0]
   return <div className="product-list-item">
   <h3>{ props.name } </h3>
   <img
@@ -12,11 +13,21 @@ export default function ProductListItem(props) {
   <div>{ props.product.description }</div>
     <div>${ props.product.price }</div>
     <div>
-      <button
-        onClick={() => props.addToCart(props.product)}
-        > Add to Cart({
-          (thisItemInCart && thisItemInCart.quantity) || 0
-        }) </button>
+      <AddBtn
+        cartItem={props.cartItem}
+        product={props.product}
+        addToCart={props.addToCart} />
+
+      {
+        props.cartItem
+         ? <RemoveBtn
+            cartItem={props.cartItem}
+            product={props.product}
+            removeFromCart={props.removeFromCart}
+            />
+          : null
+      }
+
     </div>
   </div>
 }
---
+++ src/Features/product-listing/remove-btn.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+
+export default function RemoveBtn(props) {
+  return <button
+
+    onClick={() => props.removeFromCart(props.cartItem)}
+    > Remove </button>
+}
